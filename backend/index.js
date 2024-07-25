ff1343a4c2e02bee472bc9687c296791345afc7c
bkipgaa <106474433+bkipgaa@users.noreply.github.com>
2024-07-25 10:29:56 +0300

    awai

---
+++ backend/index.js
@@ -0,0 +1,118 @@
+const port = 4000;
+const express = require ("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require ("multer");
+const path = require("path") /*getting access to backend directory*/
+const cors = require("cors");
+
+app.use(express.json()); /*Middleware to parse JSON bodies*/
+app.use(cors());
+
+// Database connection with MongoDB
+mongoose.connect("mongodb+srv://bkipgaa:P6dSy42TBGVsHXeo@cluster0.qggw3h3.mongodb.net/e-commerce")
+
+//API Create Connection with MongoDB
+app.get("/", (req,res)=>{
+    res.send("Express App is Running")
+});
+
+//Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer ({storage:storage})
+
+//Creating Upload Endpoint for images
+app.use('/images', express.static('upload/images'))
+app.post("/upload", upload.single('product'), (req,res)=>{
+    res.json({
+        sucess:1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+//Schema for creating products
+
+const Product = mongoose.model("Product",{
+    id:{ 
+        type:Number,
+        required:true,
+    },
+        name:{
+            type:String,
+            required:true
+        },
+        category:{
+            type: String,
+            required:true,
+        },
+        new_price:{
+            type:Number,
+            required:true,
+        },
+        old_price:{
+            type:Number,
+            required:true,
+        },
+        date:{
+            type:Date,
+            default:Date.now,
+        },
+    
+        available:{
+            type:Boolean,
+            default:true,
+
+        }
+    })
+    app.post('/addproduct', async(req,res)=>{
+        let products = await Product.find({}); //automatically generating id
+        let id;
+        if(products.length>0){
+            let last_product_array = products.slice(-1);
+            let last_product = last_product_array[0];
+            id = last_product.id+1;
+        }
+        else{
+            id=1;
+        }
+        const product = new Product({
+            id:id,
+            name:req.body.name,
+            image:req.body.image,
+            category:req.body.category,
+            new_price:req.body.new_price,
+            old_price:req.body.old_price
+
+        });
+        console.log(product);
+        await product.save();
+        console.log("Saved");
+        res.json({
+            success:true,
+            name:req.body.name,
+        })
+    })
+
+    //Creating API For deleting Products
+
+    app.post ('/removeproduct', async (req,res)=>{
+        
+    })
+
+
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("Server Running on Port "+port)
+    }
+    else
+    {
+        console.log("Error : " +error)
+    }
+})
