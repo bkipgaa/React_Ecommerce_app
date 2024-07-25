2ddbf84b6a461f6b94314cd0a525e38afa676a99
bkipgaa <106474433+bkipgaa@users.noreply.github.com>
2024-07-25 21:49:01 +0300

    updated

--- backend/index.js
+++ backend/index.js
@@ -101,11 +101,22 @@ const Product = mongoose.model("Product",{
 
     //Creating API For deleting Products
 
-    app.post ('/removeproduct', async (req,res)=>{
-        
+    app.post ('/removeproduct', async(req,res)=>{
+        await Product.findOneAndDelete({id:req.body.id});
+        console.log("Removed");
+        res.json({
+            success:true,
+            name:req.body.name,
+
+        })
     })
 
-
+//Creating API for getting all products
+app.get('/allproducts' , async(req,res)=>{
+    let products = await Product.find({});
+    console.log("All products Fetched");
+    res.send(products)
+})  
 
 app.listen(port,(error)=>{
     if(!error){
